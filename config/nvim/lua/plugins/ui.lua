37e62c6eb9033c08ddf807cbc3ae245dcee261bb
supermanten <supermanten8@gmail.com>
2025-11-23 09:06:44 +0530

    modify cyberdream theme

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,16 @@
 return {
 	{ "folke/tokyonight.nvim", lazy = true },
-	{ "scottmckendry/cyberdream.nvim", lazy = true },
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = true,
+		opts = {
+			variant = "auto",
+			transparent = true,
+			italic_comments = true,
+			borderless_pickers = false,
+			highlights = {
+				Comment = { fg = "#9c9c9c", bg = "NONE", italic = true },
+			},
+		},
+	},
 }
